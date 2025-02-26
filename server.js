2a417ac139f925f367c22a4eda72367f4d4dfddd
Maicom de Macedo Rodeghiero <maicom_mr@hotmail.com>
2025-02-26 19:13:40 -0300

    feat: added libs to get, crop and respose an image

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+const sharp = require('sharp');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,15 +11,35 @@ app.use(cors());
 app.use(express.json());
 
 // Rota simples
-app.get('/:gender?/:width?/:height?', (req, res) => {
+app.get('/:gender?/:width?/:height?', async (req, res) => {
     const gender = req.params.gender || 1;
     const width = req.params.width || 500;
     const height = req.params.height || 500;
 
-    res.json({
-        recebido: { gender, width, height },
-        message: 'API funcionando!'
-    });
+    const GenderEnum = {
+        1: "woman",
+        2: "men",
+        3: "other"
+    };
+    
+    genderFolder = GenderEnum[gender] || 1;
+    seedImages = 4;
+    
+    // res.json({genderFolder});
+
+    try {
+        const imagePath = path.join(`public/${genderFolder}`, `${Math.floor(Math.random() * seedImages) + 1}.png`);
+
+        // Processar a imagem
+        const croppedImage = await sharp(imagePath)
+            .resize(parseInt(width), parseInt(height))
+            .toBuffer();
+
+        res.set('Content-Type', 'image/jpeg');
+        res.send(croppedImage);
+    } catch (error) {
+        res.status(500).json({ erro: 'Erro ao processar a imagem', detalhe: error.message });
+    }
 });
 
 // Iniciar servidor
