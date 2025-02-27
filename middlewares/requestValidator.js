193a0233f9d457516aa7386d48284e135746d95e
Maicom de Macedo Rodeghiero <maicom_mr@hotmail.com>
2025-02-26 23:04:33 -0300

    feat: add max size validation

--- middlewares/requestValidator.js
+++ middlewares/requestValidator.js
@@ -1,9 +1,19 @@
 const { param, validationResult } = require('express-validator');
 
+const minWidth = 200;
+const maxWidth = 2000;
+const minHeight = 200;
+const maxHeight = 2000;
+
 exports.validateCropParams = [
 
-    param('width').isInt({ min: 200 }).withMessage('Width must be an integer greater than 200'),
-    param('height').isInt({ min: 200 }).withMessage('Height must be an integer greater than 200'),
+    param('width')
+        .isInt({ min: minWidth, max: maxWidth })
+        .withMessage(`Width must be an integer between ${minWidth} and ${maxWidth}`),
+
+    param('height')
+        .isInt({ min: minHeight, max: maxHeight })
+        .withMessage(`Height must be an integer between ${minHeight} and ${maxHeight}`),
 
     validatorHandler
 ];
