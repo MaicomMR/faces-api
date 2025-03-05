3629ed00f13c934bf51977adee486a5884b526d4
Maicom de Macedo Rodeghiero <maicom_mr@hotmail.com>
2025-03-05 19:30:01 -0300

    tests: export app and run without custom port in case of tests

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const errorHandler = require('./middlewares/errorHandler'); 
+const errorHandler = require('./middlewares/errorHandler');
 
 const router = require('./routes/routes');
 
@@ -10,11 +10,15 @@ const PORT = process.env.PORT || 3000;
 
 // Import routes
 app.use('/', router);
-app.use(errorHandler); 
+app.use(errorHandler);
 
 app.use(cors());
 app.use(express.json());
 
+if (process.env.NODE_ENV !== 'test') {
     app.listen(PORT, () => {
         console.log(`Servidor rodando em http://localhost:${PORT}`);
     });
+}
+
+module.exports = app;
