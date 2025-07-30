3fb07b101fa22349ffb4ec276fce60926120312d
Maicom M. Rodeghiero <maicom_mr@hotmail.com>
2025-07-30 14:45:57 -0300

    Update controllers/homeController.js

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,7 +2,7 @@ exports.getHome = (req, res) => {
     const baseURL = `${req.protocol}://${req.get('host')}`;
     res.status(500).json({
         mensagem: "Bem-vindo à API de processamento de imagens!",
-        instrucoes: `Para teste acessar, utilize: ${baseURL}/crop/{category}/{width}/{height}`,
+        instrucoes: `Para teste teste acessar, utilize: ${baseURL}/crop/{category}/{width}/{height}`,
         exemplo: `${baseURL}/crop/1/200/300`
     });
 };
